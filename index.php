2e1e59c6b14fa930562f8fb6476724050fb86c2f
Justin Stam <justin.stam@hva.nl>
2015-12-08 16:29:10 +0100

    achtergrond resize shizzle + map voor afbeeldingen

--- index.php
+++ index.php
@@ -1,14 +1,27 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 
 <html>
+<style>
 
+{ margin: 0; padding: 0; }
+
+html { 
+        background: url('image/awhyeah.jpg') no-repeat center center fixed; 
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+}
+
+
+</style>
 <head>
     <title>CtrlAtlDestroy</title>
-			  <link rel="shortcut icon" type="image/jpg" href="awhyeah2.jpg">
+			  <link rel="shortcut icon" type="image/jpg" href="image/awhyeah2.jpg">
 
 </head>
 
-<body background="awhyeah.jpg">
+
 
    AWH YEAH
 
