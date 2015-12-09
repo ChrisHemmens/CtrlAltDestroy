4c2c1850e14f66063a1ca1564710f53ba49369f5
Justin Stam <justin.stam@hva.nl>
2015-12-09 10:14:52 +0100

    Added tablecloth + rounded ratio to 2 decimal places

--- index.php
+++ index.php
@@ -17,13 +17,93 @@ html {
 </style>
 <head>
     <title>CtrlAtlDestroy</title>
-			  <link rel="shortcut icon" type="image/jpg" href="image/awhyeah2.jpg">
-
+	<meta name="MobileOptimized" content="width"  />
+	<link rel="shortcut icon" type="image/jpg" href="image/awhyeah2.jpg">
+			  
+    <link href="tablecloth/tablecloth.css" rel="stylesheet" type="text/css" media="screen" />
+	<script type="text/javascript" src="tablecloth/tablecloth.js"></script>
 </head>
 
 
+   <?php
+//php expert shizzle van rizzle
+$context = stream_context_create(array(
+    'http'=>array(
+        'method' => "GET",
+        'header' => "Accept-Encoding: gzip;q=0, compress;q=0\r\n",
+    )
+));
 
-   AWH YEAH
+$contents = file_get_contents('https://set7z18fgf.execute-api.us-east-1.amazonaws.com/prod/?route=getClanDetails&clanTag=%232CRCJU2V', false, $context);
+
+$jsonArray = json_decode($contents, true);
+
+echo "<img src='" . $jsonArray['clanDetails']['results']['clanBadgeImg']['l'] . "'><br />";
+echo "Clan: " . $jsonArray['clanDetails']['results']['name'] . "<br />";
+echo "Clan level: " . $jsonArray['clanDetails']['results']['clanLevel'] . "<br />";
+echo "Gewonnen oorlogen: " . $jsonArray['clanDetails']['results']['warWins'] . "<br />";
+echo "Uitleg: " . $jsonArray['clanDetails']['results']['description'] . "<br />";
+echo "Lokatie: " . $jsonArray['clanDetails']['results']['locationName'] . "<br />";
+
+?>
+<div id="log">
+<table cellspacing="0" cellpadding="0">
+  <tr>
+    <td><b>Rank</b></td>
+	<td><b>Naam</b></td>
+    <td><b>Rol</b></td> 
+	<td><b>Level</b></td>
+    <td><b>Trophies</b></td>
+	<td><b>Donaties</b></td>
+	<td><b>Ontvangen</b></td>
+	<td><b>Ratio</b></td>
+  </tr>
+  </div>
+  
+ 
+<?php
+for($i = 0; $i < $jsonArray['clanDetails']['results']['members']; $i++) {
+	$donated = $jsonArray['clanDetails']['results']['memberList'][$i]['donations'];
+	$donationsReceived = $jsonArray['clanDetails']['results']['memberList'][$i]['donationsReceived'];
+	$ratio = ($donationsReceived == 0 ? 0 : $donated/$donationsReceived);
+	$ratioColor = ($ratio >= 1 ? "green" : "red");
+	//Role translaten naar Nederlands
+	$rol = $jsonArray['clanDetails']['results']['memberList'][$i]['role'];
+	switch($rol) {
+						case 'leader':
+							$rol = 'Leider';
+						break;
+						
+						case 'member':
+							$rol = 'Lid';
+						break;
+						
+						case 'admin':
+							$rol = 'Oudste';
+						break;
+						
+						case 'coLeader':
+							$rol = 'CoLeider';
+						break;
+						
+						default:
+							$rol = 'Onbekende shizzle';
+						break;
+					}
+
+	echo "<tr>";
+		echo "<td>" . $jsonArray['clanDetails']['results']['memberList'][$i]['clanRank'] . "</td>";
+		echo "<td>" . $jsonArray['clanDetails']['results']['memberList'][$i]['name'] . "</td>"; 
+		echo "<td>" . $rol . "</td>";
+		echo "<td>" . $jsonArray['clanDetails']['results']['memberList'][$i]['expLevel'] . "</td>";
+		echo "<td>" . $jsonArray['clanDetails']['results']['memberList'][$i]['trophies'] . "</td>";
+		echo "<td>" . $donated . "</td>";
+		echo "<td>" . $donationsReceived . "</td>";
+		echo "<td bgcolor=\"". $ratioColor . "\">" .  number_format((float)$ratio, 2, '.', '') . "</td>";
+	echo "</tr>";
+}
+?>
+</table>
 
 </body>
 
